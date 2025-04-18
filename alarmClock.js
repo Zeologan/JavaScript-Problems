e4fe7ab185859103e029073045ad258b58a06ba6
unknown <rohitratate855@gmail.com>
2025-04-18 18:50:32 +0530

    create new alarm function

---
+++ alarmClock.js
@@ -0,0 +1,20 @@
+// Create a Alarm Which Console Ring... [User Define Time] 
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+const hour = prompt('Hour :');
+const min = prompt('min :');
+
+const alarm = setInterval(ringAlarm,1000);
+function ringAlarm (){
+    let time = new Date();
+    let realHour = time.getHours();
+    let realMin = time.getMinutes();
+    if(realMin == min && realHour == hour){
+        console.log(`Alarm Is Ringing .... Time is ${hour}:${min}`);
+        clearInterval(alarm);
+    }
+}
+
+
+
+
