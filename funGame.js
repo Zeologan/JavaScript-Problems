3d07964281785e8c8a6a395dd4520fa1d04ab742
Rohit Ratate <rohitratate855@gmail.com>
2025-04-21 22:44:59 +0530

    Stone, Paper, Scissor Game Added

---
+++ funGame.js
@@ -0,0 +1,41 @@
+// let create a game of stone , paper , scissor
+
+// Stone = 1  ~ 2
+// Paper = 2 ~ 3
+// Scissor = 3 ~ 1
+
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+function funGame() {
+  let round = 0;
+  let userCount = 0;
+  let compCount = 0;
+  while (round < 5) {
+    let comp = Math.ceil(Math.random() * 3);
+    let user = parseInt(prompt("Choose 1.Stone | 2.Paper | 3.Scissor : "));
+    if (user == comp) {
+      console.log("Draw");
+    } else if (user == 1 && comp == 3) {
+      userCount++;
+      console.log(`User Won This Round!... Score is : ${userCount}`);
+    } else if (user == 2 && comp == 1) {
+      userCount++;
+      console.log(`User Won This Round!... Score is : ${userCount}`);
+    } else if (user == 3 && comp == 2) {
+      userCount++;
+      console.log(`User Won This Round!... Score is : ${userCount}`);
+    } else {
+      compCount++;
+      console.log(`Computer Won This Round!... Score is : ${compCount}`);
+    }
+    round++;
+  }
+  if(userCount > compCount){
+    console.log(`User Won this Match by ${userCount} Points ..... !!!!`);
+  }else if(userCount == compCount){
+    console.log(`It's a Draw ~ User Point ${userCount} || Computer Point ${compCount}`);
+  }else{
+    console.log(`Computer Won this Match by ${compCount} Points ..... !!!!`);
+  }
+}
+funGame();
