3d30b41bf8265ab7fd4c44d0c3d127a09626f6b6
Rohit Ratate <rohitratate855@gmail.com>
2025-06-03 22:02:18 +0530

    maxSum of SubArray

---
+++ maxSumSubArray.js
@@ -0,0 +1,30 @@
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+// Brute Force ->>> 
+var maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    let startIndex, endIndex;
+    for(let i = 0; i < nums.length; i++){
+        let currentSum = 0;
+        for(let j = i; j < nums.length; j++){
+            currentSum = currentSum + nums[j];
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+                startIndex = i;
+                endIndex = j;
+            }
+        }
+    }return {
+        sums : maxSum,
+        arr : nums.slice(startIndex, endIndex + 1),
+    }
+    
+};
+console.log(maxSubArray([5,4,-1,7,8]))
